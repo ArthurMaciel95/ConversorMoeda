15abb93c53b8c6a46ca931fe846a405f0a26a0f5
Romulo Moraes <romulomoraesti@gmail.com>
2020-05-02 10:27:59 -0300

    Update

--- script.js
+++ script.js
@@ -1,97 +1,53 @@
 /*
-	CONVERSOR DE DÓLAR EM TEMPO REAL
+	CONVERSOR DE MOEDAS EM TEMPO REAL
 */
 
-/*Convertendo o dólar para o real*/
-const urlDolar = fetch('http://economia.awesomeapi.com.br/json/all/USD-BRL')
+const url = fetch('http://economia.awesomeapi.com.br/json/all')
 
+let moedaA = document.querySelectorAll('.moedaA')
+let moedaB = document.querySelectorAll('.moedaB')
 
-const dolar = document.querySelector('#dolar')
-const real = document.querySelector('#real')
-const btn = document.querySelector('#btn')
+//transforma em array para pegar o index da variavél
+let real = Array.from(moedaB)
 
-urlDolar.then((r) => {
-  return r.json()
-})
-.then((bodyDolar) => {
-  btn.addEventListener('click', mudaValor)
-    function mudaValor() {
-    const valor = bodyDolar.USD.high
+let btn = document.querySelectorAll('.btn')
 
-    dolar.value
-
-    real.innerText = (valor * dolar.value).toFixed(2).replace(".", ",")
-  }
-})
-.catch((erro) => {
-
-})
-
-/*Convertendo o real para o dólar*/
-const urlReal = fetch('http://economia.awesomeapi.com.br/json/all/USD-BRL')
-
-const moedaReal = document.querySelector('#moedaReal')
-const btnReal = document.querySelector('#btnReal')
-const moedaDolar = document.querySelector('#moedaDolar')
-
-urlReal.then((r) => {
+url.then((r) => {
 	return r.json()
-})
-.then((bodyReal) => {
-	function novoValor () {
-		const nValor = bodyReal.USD.high
+}).then((body) => {
+	//Converte o dólar para o real
+	btn[0].addEventListener('click', () => {
+		let valor = body.USD.high
+		let dolar = Array.from(moedaA)
+		real[0].innerText = (valor * dolar[0].value).toFixed(2).replace('.', ',')
 
-		moedaReal.value
+		//Não permite valores menor que zero
+		if (dolar[0].value < 0) {
+			real[0].innerText = '0'
+		}
+	})
 
-		moedaDolar.innerText = (moedaReal.value / nValor).toFixed(2).replace(".", ",")
-	}
+	//Converte o euro para o real
+	btn[1].addEventListener('click', () => {
+		let valor = body.EUR.high
+		let euro = Array.from(moedaA)
+		real[1].innerText = (valor * euro[1].value).toFixed(2).replace('.', ',')
 
-	btnReal.addEventListener('click', novoValor)
-})
+		//Não permite valores menor que zero
+		if (euro[1].value < 0) {
+			real[1].innerText = '0'
+		}
+	})
 
+	//Converte o bitcoin para o real
+	btn[2].addEventListener('click', () => {
+		let valor = body.BTC.high
+		let btc = Array.from(moedaA)
+		real[2].innerText = (valor * btc[2].value).toFixed(2).replace('.', ',')
 
-/*Convertendo euro para o real*/
-
-const urlEuro = fetch('http://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL,BTC-BRL')
-
-const euro = document.querySelector('#euro')
-const btnEuro = document.querySelector('#btnEuro')
-const moedaEuro = document.querySelector('#moedaEuro')
-
-urlEuro.then((r) => {
-	return r.json()
-})
-.then((bodyEuro) => {
-	function mudaEuro() {
-		const nEuro = bodyEuro.EUR.high
-
-		euro.value
-
-		moedaEuro.innerText = (euro.value * nEuro).toFixed(2).replace(".", ",")
-	}
-	
-	btnEuro.addEventListener('click', mudaEuro)
-})
-
-/*Converte bitcoin para o real*/
-
-const urlBitcoin = fetch('http://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL,BTC-BRL')
-
-const bitcoin = document.querySelector('#bitcoin')
-const btnBit = document.querySelector('#btnBit')
-const moedaBitcoin = document.querySelector('#moedaBitcoin')
-
-urlBitcoin.then((r) => {
-	return r.json()
-})
-.then((bodyBitcoin) => {
-	function mudaBitcoin() {
-		const nBitcoin = bodyBitcoin.BTC.high
-
-		bitcoin.value
-
-		moedaBitcoin.innerText = (bitcoin.value * nBitcoin).toFixed(2).replace(".", ",")
-	}
-	
-	btnBit.addEventListener('click', mudaBitcoin)
+		//Não permite valores menor que zero
+		if (btc[2].value < 0) {
+			real[2].innerText = '0'
+		}
+	})
 })
