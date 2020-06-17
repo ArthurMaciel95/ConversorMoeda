19c40c321f7758e7a8f7634d6cfe7ad682a9c6c9
ArthurMaciel95 <62079236+ArthurMaciel95@users.noreply.github.com>
2020-06-17 00:00:41 -0300

    Adiciona R$
    Adiciona R$ e mostra a Atualização da API na tela;

--- js/script.js
+++ js/script.js
@@ -10,7 +10,11 @@ let moedaB = document.querySelectorAll('.moedaB')
 //transforma em array para pegar o index da variavél
 let real = Array.from(moedaB)
 
+//Adiciona mais um lugar de resposta.
+let datarefresh = document.querySelectorAll('.refresh')
+
 let btn = document.querySelectorAll('.btn')
+
 
 url.then((r) => {
 	return r.json()
@@ -19,7 +23,10 @@ url.then((r) => {
 	btn[0].addEventListener('click', () => {
 		let valor = body.USD.high
 		let dolar = Array.from(moedaA)
-		real[0].innerText = (valor * dolar[0].value).toFixed(2).replace('.', ',')
+		const  refresh = body.USD.create_date
+		// Pega a data de atualização da API e Joga como resposta.
+		real[0].innerText = (valor * dolar[0].value).toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+		datarefresh[0].innerHTML = `<p>última atualização:</p>${refresh}`
 
 		//Não permite valores menor que zero
 		if (dolar[0].value < 0) {
@@ -31,8 +38,11 @@ url.then((r) => {
 	btn[1].addEventListener('click', () => {
 		let valor = body.EUR.high
 		let euro = Array.from(moedaA)
-		real[1].innerText = (valor * euro[1].value).toFixed(2).replace('.', ',')
-
+		// Pega a data de atualização da API e Joga como resposta.
+		const refresh= body.EUR.create_date
+		
+		real[1].innerText = (valor * euro[1].value).toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+		datarefresh[1].innerHTML = `<p>última atualização:</p>${refresh}`
 		//Não permite valores menor que zero
 		if (euro[1].value < 0) {
 			real[1].innerText = '0'
@@ -43,11 +53,15 @@ url.then((r) => {
 	btn[2].addEventListener('click', () => {
 		let valor = body.BTC.high
 		let btc = Array.from(moedaA)
-		real[2].innerText = (valor * btc[2].value).toFixed(2).replace('.', ',')
+		// Pega a data de atualização da API e Joga como resposta.
+		const refresh = body.BTC.create_date
 
+		real[2].innerText = (valor * btc[2].value).toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+		datarefresh[2].innerHTML = `<p>última atualização:</p>${refresh}`
 		//Não permite valores menor que zero
 		if (btc[2].value < 0) {
 			real[2].innerText = '0'
 		}
 	})
+
 })
